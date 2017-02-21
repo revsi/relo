ae779b479cec1b5fe99ace65bd4ee72f53e66737
Akirato <nurendra.choudhary@research.iiit.ac.in>
2017-02-21 09:35:09 +0530

    Production Phase 1

--- relo.py
+++ relo.py
@@ -13,21 +13,23 @@ def get_relations(text,ner_tags,lang_tag):
         for sentence in sentences:
             tokenized_sentence = punkt_word_tokenizer.tokenize(sentence)
             lemmatized_sentence = [wordnet_lemmatizer.lemmatize(token.lower()) for token in tokenized_sentence]
+            a,b,r = '','',''
             for word in lemmatized_sentence:
-                a,b,r = '','',''
                 if word in relationships:
                     r = word
-                if word in ner_tags:
+                elif word in ner_tags:
                     if a == '':
                         a = word
                     else:
                         b = word
+            if (a!='') or (b!='') or (r!=''):
+                print(lemmatized_sentence)
                 print((a,b,r))
-                if (((a != '') and (b!='')) and (r!='')):
-                    if all_relations.has_key((a,b,r)):
-                        all_relations[(a,b,r)] = all_relations[(a,b,r)]+1
-                    else:
-                        all_relations[(a,b,r)] = 1
+            if (((a != '') and (b!='')) and (r!='')):
+                if (a,b,r) in all_relations:
+                    all_relations[(a,b,r)] = all_relations[(a,b,r)]+1
+                else:
+                    all_relations[(a,b,r)] = 1
     return all_relations
                     
             
